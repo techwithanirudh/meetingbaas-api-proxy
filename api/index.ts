acdee15396c8ff104f150ded8e71ec3bda2d0d68
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-02-06 17:00:57 +0000

    fix

--- api/index.ts
+++ api/index.ts
@@ -3,24 +3,21 @@ import { handle } from 'hono/vercel'
 import { env } from 'hono/adapter'
 import { cors } from 'hono/cors'
 
-export const config = {
-  runtime: 'edge'
-}
-
+export const config = { runtime: 'edge' }
 const app = new Hono().basePath('/api')
 
 app.use('/*', cors())
+
 app.all('/*', async (c) => {
   const { API_URL } = env<{ API_URL: string }>(c as any)
-  const routePath = c.req.path.replace(/^\/api/, '');
-  const targetUrl = new URL(routePath, API_URL || "https://api.meetingbaas.com").toString();
-console.log(c.req.raw.headers)
-  const res = await fetch(targetUrl, {
+  const u = new URL(c.req.url)
+  const t = new URL(u.pathname.replace(/^\/api/, '') + u.search, API_URL || "https://api.meetingbaas.com")
+  const res = await fetch(t.toString(), {
     ...c.req.raw,
-    headers: c.req.raw.headers
-  });
-  const newResponse = new Response(res.body, res);
-  return newResponse;
-});
+    method: c.req.method,
+    headers: c.req.raw.headers,
+  })
+  return new Response(res.body, res)
+})
 
 export default handle(app)
