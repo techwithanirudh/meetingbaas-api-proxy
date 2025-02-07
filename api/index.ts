425b34e185611668703b7517b0bee5836908f148
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-02-07 07:55:51 +0000

    fix

--- api/index.ts
+++ api/index.ts
@@ -10,12 +10,15 @@ app.use('/*', cors())
 
 app.all('/*', async (c) => {
   const { API_URL } = env<{ API_URL: string }>(c as any)
+  const body = await c.req.parseBody();
   const url = new URL(c.req.url)
   const target = new URL(url.pathname.replace(/^\/api/, '') + url.search, API_URL || "https://api.meetingbaas.com")
   const res = await fetch(target.toString(), {
     ...c.req.raw,
     method: c.req.method,
     headers: c.req.raw.headers,
+    // @ts-ignore
+    body: c.req.method !== 'GET' && c.req.method !== 'HEAD' ? body : undefined, 
   })
   return new Response(res.body, res)
 })
